113524fbb054b4334815a673ad01e15e4c5d30db
SinigribovS <71001180+SinigribovS@users.noreply.github.com>
2021-11-25 00:47:22 +0300

    Update Problema_14-pagina_61.py

--- Problema_14-pagina_61.py
+++ Problema_14-pagina_61.py
@@ -1,2 +1,2 @@
-x,y=set(input('Introduceti primul sir de caractere:')),set(input('Introduceti al doilea sir de caractere:'))
+x,y=set(input('Introduceți primul șir de caractere:')),set(input('Introduceți al doilea șir de caractere:'))
 print(x.union(y),'\n',x.intersection(y),'\n',x.difference(y))
